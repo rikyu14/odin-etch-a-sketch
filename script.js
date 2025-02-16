97f31969dc9515da785c80b1248da3e0e487a687
rikyu14 <195023590+rikyu14@users.noreply.github.com>
2025-02-15 22:43:58 -0500

    Add function to create new grid

--- script.js
+++ script.js
@@ -1,14 +1,34 @@
 const container = document.querySelector('.container');
-const gridSize = 16;
+const newGridBtn = document.querySelector('.newGridBtn');
 
-// Create 16x16 grids
-for (i = 0; i < gridSize * gridSize; i++) {
-    const square = document.createElement('div');
-    square.classList.add('grid-square');
-    container.appendChild(square);
+function createGrid(size) {
+    // Clear existing squares
+    container.innerHTML = '';
 
-    square.addEventListener('mouseover', () => {
-        square.classList.add('hovered');
-    });
+    for (let i = 0; i < size * size; i++) {
+        const square = document.createElement('div');
+        square.classList.add('grid-square');
+        // Set square's width and height
+        square.style.width = `calc(100% / ${size})`;
+        square.style.height = `calc(100% / ${size})`;
+    
+        square.addEventListener('mouseover', () => {
+            square.classList.add('hovered');
+        });
+
+        container.appendChild(square);
+    }
 }
 
+createGrid(16);
+
+newGridBtn.addEventListener('click', () => {
+    let size = parseInt(prompt('Enter a number of squares per side (max = 100):'));
+
+    if (isNaN(size) || size <= 0 || size > 100) {
+        alert('Ivalid input. Please enter a number between 1 and 100.');
+        return;
+    }
+
+    createGrid(size);
+});
